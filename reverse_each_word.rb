ebd6691ebfc8df203580414a08198b07ca68a164
Nathan Fedzen <nfedzen@hotmail.com>
2020-11-22 20:09:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   new_array = sentence.split(" ")
   new_array.collect do |element|
-    element.reverse 
+    element.reverse.join(" ")
   end
-  new_array.join(" ")
+  
 end
